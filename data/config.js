08260250c0795279e77ab5ccf83362ecc18de68e
abdallah awwad <a.awwad@procabnow.com>
2019-09-09 01:50:37 +0200

    first changes

--- data/config.js
+++ data/config.js
@@ -1,17 +1,15 @@
 module.exports = {
-	defaultTitle: 'John Doe',
+	defaultTitle: 'Abdallah Awwad',
 	logo: 'https://portfolio.smakosh.com/favicon/favicon-512.png',
-	author: 'John Doe',
-	url: 'https://portfolio.smakosh.com',
-	legalName: 'John Doe',
-	defaultDescription: 'I’m John and I’m a Backend & Devops engineer!',
+	author: 'Abdallah Awwad',
+	url: 'https://arawwad.codes',
+	legalName: 'Abdallah Awwad',
+	defaultDescription: 'I’m Abdallah and I’m a Frontend engineer!',
 	socialLinks: {
-		twitter: 'http://www.twitter.com/smakosh',
-		github: 'https://github.com/smakosh',
-		linkedin: 'https://www.linkedin.com/in/ismail-ghallou-630149122/',
-		instagram: 'https://instagram.com/smakosh19',
-		youtube: 'https://www.youtube.com/user/smakoshthegamer',
-		google: 'https://plus.google.com/u/0/+IsmailSmakoshGhallou',
+		twitter: 'http://www.twitter.com/DreadfulBadr',
+		github: 'https://github.com/arawwad',
+		linkedin: 'https://www.linkedin.com/in/abdallah-awwad/',
+		instagram: 'https://instagram.com/awad6825',
 	},
 	googleAnalyticsID: 'UA-88875900-4',
 	themeColor: '#6b63ff',
@@ -31,5 +29,5 @@ module.exports = {
 		phone: 'phone number',
 	},
 	foundingDate: '2018',
-	recaptcha_key: '6Lcs6lQUAAAAAEwhNH2IsobIe2csdda4TU3efpMN'
+	recaptcha_key: '6LdOUbcUAAAAAAX6eH6PkjGjPSfIXw9oqa9ZRpD5'
 }
